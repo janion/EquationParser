d9df053554bc9d65da22ecfbbbe4943c6c061621
janion <erik_@hotmail.co.uk>
2018-05-04 13:48:30 +0100

    Rationalised the package structure for EquationParser. Tests in equation parser may no longer work. Can be fixed later

---
+++ test/FlatTestArray.py
@@ -0,0 +1,26 @@
+'''
+Created on 13 Jun 2016
+
+@author: Janion
+'''
+import wx
+
+class Window(wx.Frame):
+    
+    SIZE = 10
+    
+    def __init__(self, parent, idd, title):
+        wx.Frame.__init__(self, parent, idd, title, size=(30 * (self.SIZE + 1), 30 * (self.SIZE + 2)))
+        self.panel = wx.Panel(self, -1)
+        self.SetMinSize(self.GetSize())
+        
+        self.btns = [[None for y in xrange(self.SIZE)] for x in xrange(self.SIZE)]
+        
+        for x in xrange(self.SIZE):
+            for y in xrange(self.SIZE):
+                self.btns[x][y] = wx.StaticText(self.panel, -1, '', pos=(10 + (30 * x), 10 + (30 * y)), size=(25, 25))
+                self.btns[x][y].Enable(False)
+                self.btns[x][y].SetBackgroundColour(wx.LIGHT_GREY)
+    
+    def setColour(self, position, red, green, blue):
+        self.btns[position[0]][position[1]].SetBackgroundColour((red, green, blue))
---
+++ test/Launcher.py
@@ -0,0 +1,69 @@
+'''
+Created on 8 Apr 2016
+
+@author: Janion
+'''
+
+import time
+import wx
+from threading import Thread
+
+from FlatTestArray import Window
+from eqn.EquationParser import Equation
+
+
+################################################################################
+
+def displayFunction(window, functionText):
+    function = Equation(functionText)
+    start = time.time()
+    exitThread = False
+    
+    while True:
+        now = time.time()
+        for x in xrange(window.SIZE):
+            for y in xrange(window.SIZE):
+                val = function.evaluate(x, y, now - start)
+                try:
+                    window.IsActive()
+                    if len(val) > 0:
+#                         wx.CallAfter(window.setColour, [x, y], 0, 127 * val[0], 0)
+                        val = 127 * val[0]
+                        wx.CallAfter(window.setColour, [x, y], 0, val, 255 - val)
+                    else:
+                        wx.CallAfter(window.setColour, [x, y], 0, 0, 0)
+                except:
+                    exitThread = True
+                    break
+            if exitThread:
+                break
+
+        wx.CallAfter(window.Refresh)
+        
+        if not exitThread:
+            time.sleep(0.1)
+        
+################################################################################
+
+if __name__ == '__main__':
+    app = wx.App()
+    
+    # # Folding lines?
+    # function = "sin(t + ((x - 5) + (t * (y - 5)))) + 1"
+    # Outward propagating sine rings
+    function = "sin(sqrt(((x - 4.5) * (x - 4.5)) + ((y - 4.5) * (y - 4.5))) - (3 * t)) + 1"
+#     # Oscillating sine rings
+#     function = "sin(sqrt(((x - 4.5) * (x - 4.5)) + ((y - 4.5) * (y - 4.5))) - (2 * sin(t))) + 1"
+#     # Diagonal moving waves
+#     function = "sin(((x - 4.5) + (y - 4.5)) - (8 * t)) + 1"
+#     # Spinning lines
+#     function = "sin(((x - 4.5) * sin(t)) + ((y - 4.5) * cos(t))) + 1"
+
+    # Create a window
+    window = Window(None, -1, function)
+    window.Show()
+
+    workThread = Thread(target = displayFunction, args=(window, function) )
+    workThread.start()
+    
+    app.MainLoop()
